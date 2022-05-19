2005ab0ad02573ae2ee2f2818b758145bcf1a511
Jack Baty <jack@baty.net>
2022-05-19 09:45:05 -0400

    Add doing.sh

---
+++ doing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Doing
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🕥
+
+# Documentation:
+# @raycast.description Add entry using Doing
+# @raycast.author Jack Baty
+# @raycast.authorURL https://baty.net
+# @raycast.argument1 { "type": "text", "placeholder": "What are you doing?" }
+
+
+doing now $1
+
